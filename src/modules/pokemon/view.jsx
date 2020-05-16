482a2b22eb6df0032978eace9acae32f6cd779e8
Andarias Silvanus <asilvanus5@gmail.com>
2020-05-16 12:47:03 +0700

    add pokemon view

---
+++ src/modules/pokemon/view.jsx
@@ -0,0 +1,84 @@
+import React, { useEffect } from "react";
+import T from "prop-types";
+import { Select, OptionType } from "../../components/select/select";
+import { Modal } from "../../components/modal/modal";
+import { PokemonCard } from "../../components/pokemon-card/pokemon-card";
+import { PokemonTextThumbnail } from "../../components/pokemon-text-thumbnail/pokemon-text-thumbnail";
+import { getUrlValue } from "../../utils/getUrlValue";
+
+const propTypes = {};
+
+export const PokemonView = ({
+  openModal,
+  list,
+  detail,
+  selectedFilterOption,
+  pokemonTypes,
+
+  handleSelectChange,
+  handlePokemonThumbnailClicked,
+  handlePokemonDetailClosed,
+  updateOffset,
+}) => {
+  const selectOptions = pokemonTypes.map((e) => ({
+    value: getUrlValue(e.url),
+    label: e.name,
+  }));
+
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop !==
+      document.documentElement.offsetHeight
+    )
+      return;
+    updateOffset();
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div>
+      <Select
+        options={selectOptions}
+        onChange={handleSelectChange}
+        selectedOption={selectedFilterOption}
+        label="Filter by types"
+      />
+      {list.map((e, i) => (
+        <PokemonTextThumbnail
+          text={e.name}
+          onClick={() => handlePokemonThumbnailClicked(getUrlValue(e.url))}
+        />
+      ))}
+      <PokemonDetailSection
+        openModal={openModal}
+        handlePokemonDetailClosed={handlePokemonDetailClosed}
+        detail={detail}
+      />
+    </div>
+  );
+};
+
+const PokemonDetailSection = ({
+  openModal,
+  handlePokemonDetailClosed,
+  detail,
+}) => {
+  console.log("detaill", detail);
+  if (detail) {
+    const {
+      sprites: { front_default },
+    } = detail;
+    return (
+      <Modal isOpen={openModal} handleClose={handlePokemonDetailClosed}>
+        <PokemonCard image={front_default} />
+      </Modal>
+    );
+  }
+  return null;
+};
+
+PokemonView.propTypes = propTypes;
