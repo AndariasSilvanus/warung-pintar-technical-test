9f4ee7caa0757f4903987b1f8ceca6ffaf1def1e
Andarias Silvanus <asilvanus5@gmail.com>
2020-05-16 12:44:20 +0700

    add UI card

---
+++ src/components/card/card.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import MaterialUICard from "@material-ui/core/Card";
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+export const Card = ({ children }) => {
+  const classes = useStyles();
+
+  return <MaterialUICard className={classes.root}>{children}</MaterialUICard>;
+};
+
+export default Card;
