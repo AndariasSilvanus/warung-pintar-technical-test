b703a22431e8fd744d7b6657fee3c7ad78a98f0d
Andarias Silvanus <asilvanus5@gmail.com>
2020-05-16 12:46:57 +0700

    add pokemon container

---
+++ src/modules/pokemon/container.jsx
@@ -0,0 +1,97 @@
+import React, { useReducer, useEffect } from "react";
+import { pokeService } from "../../services";
+import {
+  DATA_ACTION_TYPE,
+  UI_ACTION_TYPE,
+  dataInitialState,
+  uiInitialState,
+  dataReducer,
+  uiReducer,
+} from "./reducer";
+
+async function fetchPokemonDetail(pokemon) {
+  const [data, error] = await pokeService.getPokemonData(pokemon);
+  return [data, error];
+}
+
+async function fetchPokemonListByType(type) {
+  const [data, error] = await pokeService.getPokemonListByType(type);
+  return [data, error];
+}
+
+export const PokemonContainer = ({ children }) => {
+  // states
+  const [{ openModal }, uiDispatch] = useReducer(uiReducer, uiInitialState);
+  const [
+    { list, offset, limit, detail, selectedFilterOption, pokemonTypes },
+    dataDispatch,
+  ] = useReducer(dataReducer, dataInitialState);
+
+  // methods
+  const handleSelectChange = async (type, idx) => {
+    const [data, error] = await fetchPokemonListByType(type);
+    if (data) {
+      dataDispatch({
+        type: DATA_ACTION_TYPE.REPLACE_LIST,
+        data: data.pokemon.map((e) => e.pokemon),
+      });
+      dataDispatch({ type: DATA_ACTION_TYPE.RESET_OFFSET });
+    }
+  };
+
+  const handlePokemonThumbnailClicked = async (pokemon) => {
+    uiDispatch({ type: UI_ACTION_TYPE.UPDATE_MODAL, data: true });
+    const [data, error] = await fetchPokemonDetail(pokemon);
+    if (data) {
+      dataDispatch({ type: DATA_ACTION_TYPE.UPDATE_POKEMON_DETAIL, data });
+    }
+  };
+
+  const handlePokemonDetailClosed = () => {
+    uiDispatch({ type: UI_ACTION_TYPE.UPDATE_MODAL, data: false });
+  };
+
+  const updateOffset = () => {
+    dataDispatch({ type: DATA_ACTION_TYPE.UPDATE_OFFSET });
+  };
+
+  // lifecycle
+  useEffect(() => {
+    async function fetchPokemonList() {
+      const [data, error] = await pokeService.getPokemonList(limit, offset);
+      if (data) {
+        dataDispatch({
+          type: DATA_ACTION_TYPE.APPEND_LIST,
+          data: data.results,
+        });
+      }
+    }
+    fetchPokemonList();
+  }, [offset]);
+
+  useEffect(() => {
+    async function fetchPokemonTypes() {
+      const [data, error] = await pokeService.getAllPokemonTypes();
+      dataDispatch({
+        type: DATA_ACTION_TYPE.UPDATE_POKEMON_TYPES,
+        data: data.results,
+      });
+    }
+    fetchPokemonTypes();
+  }, []);
+
+  return children({
+    // states
+    openModal,
+    list,
+    detail,
+    selectedFilterOption,
+    pokemonTypes,
+
+    // methods
+    handleSelectChange,
+    handlePokemonThumbnailClicked,
+    handlePokemonDetailClosed,
+    updateOffset,
+  });
+};
